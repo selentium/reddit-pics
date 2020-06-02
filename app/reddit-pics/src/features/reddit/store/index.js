3cc1327f061dc5638321147580581e17b9b2e5c0
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2020-06-02 13:55:47 +0500

    Store configuration

--- app/reddit-pics/src/features/reddit/store/index.js
+++ app/reddit-pics/src/features/reddit/store/index.js
@@ -1,23 +1,21 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import rootReducer from '../reducers'
 
 
 
 
 // We'll use redux-logger just as an example of adding another middleware
-import logger from 'redux-logger'
-
-import {postSlice} from '../reducers'
-
-//THIS IS WRONG!!!
-const reducer = {
-    posts: postSlice
-}
-
-const middleware = [...getDefaultMiddleware(), logger]
+//import logger from 'redux-logger'
 
 
-const store = configureStore({
-    reducer,
-    middleware,
-    devTools: process.env.NODE_ENV !== 'production'
+
+
+const middleware = [...getDefaultMiddleware()]
+
+
+const  store = configureStore({
+    reducer: rootReducer,
+    middleware: middleware
 });
+
+export default store;
---
+++ app/reddit-pics/src/features/reddit/store/index.test.js
@@ -0,0 +1,6 @@
+import store from './index.js'
+import { truncateSync } from 'fs';
+
+test('dummy', () => {
+    expect(2 + 2).toBe(4);
+});
